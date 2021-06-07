e9b58368897f83af54f0ddbb21e424f0f1e51901
mingi <meengi07@gmail.com>
2021-06-07 18:13:57 +0900

    coding test - 1day

--- CodingTest_Team_NTG/src/com/CodingTest/KMG/run/Run.java
+++ CodingTest_Team_NTG/src/com/CodingTest/KMG/run/Run.java
@@ -1,10 +1,23 @@
-package com.CodingTest.KDH.run;
+package com.CodingTest.KMG.run;
 
 public class Run {
 
+	public static void main1(String[] args){
+		//백준 2557문제 
+        System.out.println("Hello World!");
+    }
+	
+	public static void main2(String[] args){
+		//백준 10718문제 
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+  }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	    //백준 10171문제 
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");    
+		
 	}
-
 }
