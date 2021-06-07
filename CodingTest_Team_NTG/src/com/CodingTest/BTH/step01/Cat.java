cc420dd3cdfbf3ff6581fed3248e508ceef829fe
Furaha-DH <pastelto@naver.com>
2021-06-07 23:08:40 +0900

    0.0 error fixed

---
+++ CodingTest_Team_NTG/src/com/CodingTest/BTH/step01/Cat.java
@@ -0,0 +1,14 @@
+package com.CodingTest.BTH.step01;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
