9442d866b8b2c67af7ed1239252e2b5830abe8ca
kh-b1 <zmfozjko0465@gmail.com>
2021-06-07 19:24:33 +0900

    커밋 테스트 중입니다.

---
+++ CodingTest_Team_NTG/src/com/CodingTest/BTH/step01/Cat.java
@@ -0,0 +1,16 @@
+package com.CodingTest.BTH.step01;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	
+
+	}
+
+}
