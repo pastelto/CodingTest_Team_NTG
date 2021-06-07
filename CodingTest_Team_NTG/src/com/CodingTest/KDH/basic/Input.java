353a11ac258d221e134e3b6bda9be4b3e4bd0351
Furaha-DH <pastelto@naver.com>
2021-06-07 23:01:20 +0900

    1. 0607 입출력 01~03

---
+++ CodingTest_Team_NTG/src/com/CodingTest/KDH/basic/Input.java
@@ -0,0 +1,22 @@
+package com.CodingTest.KDH.basic;
+
+public class Input {
+
+	  public void input01() {// 백준 문제 제출 형식 (가운데 static 꼭 필수로 적어주기!)
+		        System.out.println("Hello World!");
+		}
+	  
+	  public void input02() {
+			  System.out.println("강한친구 대한육군");
+			  System.out.println("강한친구 대한육군");
+		  
+	  }
+	  
+	  public void input03(){
+		  	System.out.println("\\    /\\");
+			System.out.println(" )  ( ')");
+			System.out.println("(  /  )");
+			System.out.println(" \\(__)|");
+	  }
+	
+}
