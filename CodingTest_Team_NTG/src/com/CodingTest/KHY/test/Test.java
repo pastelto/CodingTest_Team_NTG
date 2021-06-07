591b3002aebe5b2bb851ce2a85ffc928989db95e
KHY0705 <khyaa0705@gmail.com>
2021-06-07 18:15:22 +0900

    0607_codingTest

---
+++ CodingTest_Team_NTG/src/com/CodingTest/KHY/test/Test.java
@@ -0,0 +1,17 @@
+package com.CodingTest.KHY.test;
+
+public class Test {
+	public void test1(){
+		System.out.println("Hello World!");
+	}
+	public void test2() {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+	public void test3() {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
