14b74525bb6c3253f1bf7c164ed1b56a1ef6a9ef
mingi <meengi07@gmail.com>
2021-06-09 00:46:41 +0900

    coding test 2day

--- CodingTest_Team_NTG/src/com/CodingTest/KMG/run/Run.java
+++ CodingTest_Team_NTG/src/com/CodingTest/KMG/run/Run.java
@@ -1,5 +1,5 @@
 package com.CodingTest.KMG.run;
-
+import java.util.*;
 public class Run {
 
 	public static void main1(String[] args){
@@ -12,7 +12,7 @@ public class Run {
         System.out.println("강한친구 대한육군");
         System.out.println("강한친구 대한육군");
   }
-	public static void main(String[] args) {
+	public static void main3(String[] args) {
 	    //백준 10171문제 
 		System.out.println("\\    /\\");
 		System.out.println(" )  ( ')");
@@ -20,4 +20,32 @@ public class Run {
 		System.out.println(" \\(__)|");    
 		
 	}
+	public static void main4(String[] args) {
+		//백준 10172문제 
+    System.out.println("|\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.println("||_/=\\\\__|");
+	}
+	//import java.util.*;
+		//public class Main{
+	public static void main5(String[] args) {
+		//백준 1000문제 
+		Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    System.out.println(a+b);
+	}
+	//import java.util.*;
+	//public class Main{
+	    public static void main6(String[] args) {
+			Scanner sc = new Scanner(System.in);
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    System.out.println(a-b);
+	//	}    
+	}
+
+	
 }
