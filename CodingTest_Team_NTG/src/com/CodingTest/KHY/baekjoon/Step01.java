19b1a29b178a73aad1a09d5c537a4b71897648d4
KHY0705 <khyaa0705@gmail.com>
2021-06-08 17:52:30 +0900

    20210608 baekjoon Step01-no.4~6 코드 업로드

---
+++ CodingTest_Team_NTG/src/com/CodingTest/KHY/baekjoon/Step01.java
@@ -0,0 +1,44 @@
+package com.CodingTest.KHY.baekjoon;
+
+import java.util.Scanner;
+
+public class Step01{
+	//2021-06-07
+	public void test1(){
+		System.out.println("Hello World!");
+	}
+	public void test2() {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+	public void test3() {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+	//2021-06-08
+	public void test4() {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+	public void test5() {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+	}
+	public void test6() {
+Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a-b);
+	}
+}
