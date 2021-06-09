ab71b8cc40518412f60928238c05f724abc70a1c
DH_furaha <80407403+pastelto@users.noreply.github.com>
2021-06-09 14:01:28 +0900

    Revert "Revert "Merge pull request #22 from pastelto/sowon""
    This reverts commit 497af97448c1543b767cac813ae87ef5d8b6f89c.

---
+++ CodingTest_Team_NTG/src/com/CodingTest/KSW/baekjoon/step1/AmulitiB.java
@@ -0,0 +1,23 @@
+package com.CodingTest.KSW.baekjoon.step1;
+
+import java.util.Scanner;
+
+public class AmulitiB {
+
+	public static void main(String[] args) {
+		
+		// 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a;
+		int b;
+
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a*b);
+		
+	}
+
+}
