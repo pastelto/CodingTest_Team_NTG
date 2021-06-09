d910d0d28d3d4b90c121ebdd1fc74600dd453e1c
mingi <meengi07@gmail.com>
2021-06-09 23:14:14 +0900

    coding test 3days

--- CodingTest_Team_NTG/src/com/CodingTest/KMG/run/Run.java
+++ CodingTest_Team_NTG/src/com/CodingTest/KMG/run/Run.java
@@ -1,10 +1,105 @@
 package com.CodingTest.KMG.run;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 public class Run {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main1(String[] args){
+		//백준 2557문제 
+        System.out.println("Hello World!");
+    }
+	
+	public static void main2(String[] args){
+		//백준 10718문제 
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+  }
+	public static void         main3(String[] args) {
+	    //백준 10171문제 
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");    
+	}	
 
+	public static void main4(String[] args) {
+		//백준 10172문제 
+    System.out.println("|\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.println("||_/=\\\\__|");
 	}
-
+	//import java.util.*;
+		//public class Main{
+	public static void main5(String[] args) {
+		//백준 1000문제 
+		Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    System.out.println(a+b);
+	}
+	//import java.util.*;
+	//public class Main{
+	 public static void main6(String[] args) {
+	    	//백준 1001문제 
+		 Scanner sc = new Scanner(System.in);
+		 int a = sc.nextInt();
+		 int b = sc.nextInt();
+		 System.out.println(a-b);
+	//	}    
+	}
+	
+	
+	public static void main7(String[] args) {
+	 		 //백준 10998문제 
+		Scanner sc = new Scanner(System.in);
+	 	int a = sc.nextInt();
+	 	int b = sc.nextInt();
+	 	System.out.println(a * b);
+	 	}
+	 public static void main8(String[] args) {
+		 	//백준 1008문
+			Scanner sc = new Scanner(System.in);
+		    double a = sc.nextDouble();
+		    double b = sc.nextDouble();
+		    System.out.println(a / b);
+		}   
+	 public static void main9(String[] args) {
+		 //백준 10869문제 
+		 Scanner sc = new Scanner(System.in);
+		 int a = sc.nextInt();
+		 int b = sc.nextInt();
+         System.out.println(a + b);
+         System.out.println(a - b);
+         System.out.println(a * b);
+		 System.out.println(a / b);
+         System.out.println(a % b);
+		}       
+	 public static void main10(String[] args) {
+		 //백준 10430문제 
+		 Scanner sc = new Scanner(System.in);
+		 int a = sc.nextInt();
+		 int b = sc.nextInt();
+		 int c = sc.nextInt();    
+         System.out.println((a + b) % c);
+         System.out.println(((a % c) + (b % c)) % c);
+         System.out.println((a * b) % c);
+         System.out.println(((a % c) * (b % c)) % c);
+		}       
+	public static void main11(String[] args) {
+		//백준 2588문제 
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a * (b % 10 ));
+		System.out.println(a * ((b % 100) / 10));
+		System.out.println(a * (b / 100));
+		System.out.println(a * b);
+		
+		
+	}
 }
+
