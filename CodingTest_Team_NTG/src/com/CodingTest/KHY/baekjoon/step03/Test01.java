57258e51a89bd17faea39b36ee1b14a5f9208ab7
KHY0705 <khyaa0705@gmail.com>
2021-06-14 14:39:48 +0900

    0614 백준 step02 no.05/ step03 no.01~02

---
+++ CodingTest_Team_NTG/src/com/CodingTest/KHY/baekjoon/step03/Test01.java
@@ -0,0 +1,18 @@
+package com.CodingTest.KHY.baekjoon.step03;
+
+import java.util.Scanner;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(num + " * " + i + " = " + (num * i));
+		}
+
+	}
+
+}
