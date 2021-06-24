7ae055bc9ea1013ab40d4ba8bf093ad043448763
Kim sowon <nare20027@gmail.com>
2021-06-25 08:47:42 +0900

    01 0625 코딩테스트 소원

---
+++ CodingTest_Team_NTG/src/com/CodingTest/KSW/baekjoon/step7/Repeat.java
@@ -0,0 +1,28 @@
+package com.CodingTest.KSW.baekjoon.step7;
+
+import java.util.Scanner;
+
+public class Repeat {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+	
+		for(int i = 0; i < t; i++) { // 새로운 문자열을 만들 횟수
+			
+			int r = sc.nextInt(); // 문자열의 문자를 반복할 횟수
+			String s = sc.next(); // 문자열 받기
+			
+			for(int j = 0; j < s.length(); j++) { // 문자열의 길이만큼 반복
+				for(int k = 0; k < r; k++) { // r 만큼 문자를 반복해서
+					System.out.print(s.charAt(j)); // 새로운 문자열로 출력
+				}
+			}
+			System.out.println(); // 케이스 마다 한 칸 띄우기
+		}
+
+	}
+
+}
